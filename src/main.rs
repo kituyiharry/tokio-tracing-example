feca5caf8d1861781ef71ac95482ba0b4bf60d56
harryk <kituyiharry@gmail.com>
2022-03-24 20:04:12 +0800

    Tokio Non-blocking io

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,22 @@
-use std::{
-    error::Error,
-    io::{Read, Write},
-    net::{SocketAddr, TcpListener, TcpStream},
-};
+use std::error::Error;
+use std::net::SocketAddr;
+
+use tokio::net::TcpListener;
+
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let addr: SocketAddr = "0.0.0.0:3779".parse()?;
-    let listener = TcpListener::bind(addr)?;
-    println!("Listening on http://{}", addr);
-    loop {
-        let (stream, addr) = listener.accept()?;
-        std::thread::spawn(move || {
-            if let Err(e) = handle_connection(stream, addr) {
-                println!("Error handling connection {addr}: {e}");
-            }
-        });
-    }
-}
-
-fn handle_connection(mut stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
-    println!("Accepted connection from {addr}");
-
-    let mut incoming = vec![];
-
-    loop {
-        let mut buf = vec![0u8; 1024];
-        let read = stream.read(&mut buf)?;
-        incoming.extend_from_slice(&buf[..read]);
-
-        if incoming.len() > 4 && &incoming[incoming.len() - 4..] == b"\r\n\r\n" {
-            break;
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?;
+    rt.block_on(async {
+        let addr: SocketAddr = "0.0.0.0:3779".parse()?; 
+        println!("Listening on http://{}", addr);
+        let listener = TcpListener::bind(addr).await?;
+        loop {
+            let (stream, addr) = listener.accept().await?;
+            println!("Accepted connection from {addr}");
+            // do nothing for now, it's a simple example
+            drop(stream)
         }
-    }
-
-    let incoming = std::str::from_utf8(&incoming)?;
-    println!("Got HTTP request:\n{}", incoming);
-    stream.write_all(b"HTTP/1.1 200 OK\r\n")?;
-    stream.write_all(b"\r\n")?;
-    stream.write_all(b"Hello from plaque!\n")?;
-    println!("Closing connection for {addr}");
-
-    Ok(())
+    })
 }
